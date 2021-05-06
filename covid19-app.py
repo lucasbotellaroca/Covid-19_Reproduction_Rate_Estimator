931c68468f252b8d7416c7b16a421e81367db0f0
lucasbotellaroca <71489078+lucasbotellaroca@users.noreply.github.com>
2021-05-06 23:37:17 +0200

    Add files via upload

--- covid19-app.py
+++ covid19-app.py
@@ -8,21 +8,23 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Title
 st.title('Covid-19 Forecast App')
-st.text('Check how Covid-19 spreads weekly.')
+st.text('Check how Covid-19 spreads weekly based on features')
 
 
 show_info = st.checkbox('Show Fields Description')
 if show_info:
 	st.subheader('Fields Description (Mean of week)')
-	st.markdown('**Bad Mobility**: Refers to the increase or decrease of visitors in retail and recreation, transit stations, grocery, pharmacy and workplaces.')
-	st.markdown('**Good Mobility**: Refers to the increase or decrease of visitors in parks and time spent at home.')
+	st.markdown('**Mobility Index**: Refers to the increase or decrease of visitors in retail and recreation, transit stations, grocery, pharmacy and workplaces.')
+	st.markdown('**Residential**: Refers to the increase or decrease time spent at home.')
+	st.markdown('**Awareness Measures**: Governments action on facial coverings and public information campaigns. (0. None, 1. Low, 2. Medium, 3. High, 4.Very high)')
 	st.markdown('**Economic Measures**: Governments support to debt relief and income support. (0. None, 1. Low, 2. Medium, 3. High)')
-	st.markdown('**Health Measures**: Masks requirements and facial coverings. (0. None, 1. Low, 2. Medium, 3. High)')
-	st.markdown('**Mobility Measures**: Governments restrictions in gatherings, lockdowns, cancelation of public events, restriction in internal movements, close public transport, workplace and school closures. (0. None, 1. Low, 2. Medium, 3. High)')
+	st.markdown('**Health Measures**: Governments action on contact tracing and testing policies (0. None, 1. Low, 2. Medium, 3. High)')
+	st.markdown('**Mobility Measures**: Governments restrictions in gatherings, lockdowns, restriction in internal movements, close public transport. (0. None, 1. Low, 2. Medium, 3. High)')
+	st.markdown('**Closure Measures**: Governments closure of workplaces, school and cancelation of public events. (0. None, 1. Low, 2. Medium, 3. High)')
 	st.markdown('**Holiday**: Number of holidays in the past week')
 	st.markdown('**Temperature**: Temperature in celsius')
-	st.markdown('**Precipitation**: Precipitation in mm')
-	st.markdown('**Number of arrivals**: Toursim arrivals average (2015-2019)')
+	st.markdown('**Precipitation**: Precipitation in mm/h')
+	st.markdown('**Number of arrivals**: Tourism arrivals average (2015-2019)')
 	st.markdown('**Urban Population**: Percentaje of the population that lives in urban areas')
 	st.markdown('**Vaccinations**: Number of vaccine doses administered per 100')
 	st.markdown('**Youth Unemployment**: Percentaje of youth unemployment')
@@ -33,25 +35,29 @@ if show_info:
 st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
-    bad_mobility = st.sidebar.slider('Bad Mobility', -100, 100, -20)
-    good_mobility = st.sidebar.slider('Good Mobility', -100, 100, 10)
-    government_economic_measures = st.sidebar.slider('Economic Measures', 0, 3, 1)
+    mobility_index = st.sidebar.slider('Bad Mobility', -100, 100, -20)
+    residential = st.sidebar.slider('Residential', -100, 100, 10)
+    awareness_measures = st.sidebar.slider('Awareness Measures', 0, 4, 1)
     health_measures = st.sidebar.slider('Health Measures', 0, 3, 1)
     mobility_measures = st.sidebar.slider('Mobility Measures',0, 3, 1)
-    holiday = st.sidebar.slider('Holiday', 0, 3, 0)
+    economic_measures = st.sidebar.slider('Economic Measures',0, 3, 1)
+    closure_measures = st.sidebar.slider('Closure Measures',0, 3, 1)
+    holiday = st.sidebar.slider('Holiday', 0, 7, 0)
     temp = st.sidebar.slider('Temperature', -26, 40, 15)
     prcp = st.sidebar.slider('Precipitation', 0, 2, 0)
     number_of_arrivals = st.sidebar.slider('Number of arrivals', 121000, 82570000, 13284934)
     urban_population = st.sidebar.slider('Urban Population', 30, 100, 72)
     total_vaccinations_per_100 = st.sidebar.slider('Total Vaccinations per 100',0, 200, 0)
     youth_unemployment = st.sidebar.slider('Youth Unemployment',0, 35, 10)
-    infected_value = st.sidebar.slider('Infections', 0, 50, 5)
+    infections_value = st.sidebar.slider('Infections', 0, 200, 20)
     accumulated = st.sidebar.slider('Accumulated', 0, 100, 5)
-    data = {'bad_mobility': bad_mobility,
-            'good_mobility': good_mobility,
-            'government_economic_measures': government_economic_measures*70,
-            'health_measures': health_measures*80,
-            'mobility_measures': mobility_measures*700,
+    data = {'mobility_index': mobility_index,
+            'residential': residential,
+            'awareness_measures': awareness_measures,
+            'health_measures': health_measures,
+            'mobility_measures': mobility_measures,
+            'economic_measures': economic_measures,
+            'closure_measures': closure_measures,
             'holiday': holiday,
             'temp': temp,
             'prcp': prcp,
@@ -59,29 +65,35 @@ def user_input_features():
             'urban_population': urban_population,
             'total_vaccinations_per_100': total_vaccinations_per_100,
             'youth_unemployment':youth_unemployment,
-            'infected_value': infected_value,
-            'accumulated': accumulated}
+            'infections_value': infections_value,
+            'accumulated': accumulated/100}
     features = pd.DataFrame(data, index=[0])
     return features
 
 s = user_input_features()
+
+print(shap.__version__)
+
 def st_shap(plot, height=None):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
 
-pickle_file = './model_regGB.pickle'
-if st.sidebar.button('Calculate Reproduction Rate'):
+pickle_file = './model_hist_reg'
+scaler_file = './scaler'
+if st.sidebar.button('Calculate Estimated Reproduction Rate'):
 	st.subheader('Specified Input parameters')
 	st.write(s)
+	scaler = pickle.load(open(scaler_file,'rb'))
+	df_scaled=pd.DataFrame(scaler.transform(s), columns=s.columns)
 	model = pickle.load(open(pickle_file,'rb'))
-	model_predict = (model.predict(s)).astype(str)
+	model_predict = (model.predict(df_scaled)).astype(str)
 	st.markdown('**Estimated Reproduction Rate:**')
 	st.write(model_predict[0])
 
 	st.markdown('**Feature importance based on SHAP values**')
 	explainerModel = shap.TreeExplainer(model)
-	shap_values_Model = explainerModel.shap_values(s)
-	st_shap(shap.force_plot(explainerModel.expected_value, shap_values_Model[0], s.iloc[[0]]), 125)
+	shap_values_Model = explainerModel.shap_values(df_scaled)
+	st_shap(shap.force_plot(explainerModel.expected_value, shap_values_Model[0], df_scaled.iloc[[0]]), 125)
 	shap.summary_plot(shap_values_Model, s, plot_type="bar")
 	st.pyplot(bbox_inches='tight')
 
