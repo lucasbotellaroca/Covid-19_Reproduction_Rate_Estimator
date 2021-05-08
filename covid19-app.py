6c231a97f0834a98dd19d949020031d12de62849
lucasbotellaroca <71489078+lucasbotellaroca@users.noreply.github.com>
2021-05-08 17:13:53 +0200

    Add files via upload

--- covid19-app.py
+++ covid19-app.py
@@ -6,6 +6,8 @@ import shap
 import streamlit.components.v1 as components
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+st.image('https://i.ibb.co/crCVSHp/Screenshot-2021-05-08-at-10-43-19.png')
+
 # Title
 st.title('Covid-19 Forecast App')
 st.text('Check how Covid-19 spreads weekly based on features')
@@ -16,10 +18,9 @@ if show_info:
 	st.subheader('Fields Description (Mean of week)')
 	st.markdown('**Mobility Index**: Refers to the increase or decrease of visitors in retail and recreation, transit stations, grocery, pharmacy and workplaces.')
 	st.markdown('**Residential**: Refers to the increase or decrease time spent at home.')
-	st.markdown('**Awareness Measures**: Governments action on facial coverings and public information campaigns. (0. None, 1. Low, 2. Medium, 3. High, 4.Very high)')
+	st.markdown('**Infections**: An indicator of the level of virality in the current week (0. None, 5. Low, 10. Medium, 15. High, 20-25. Very High)')
+	st.markdown('**Accumulated cases**: Indicator of excess deaths up to date, indicator of population inmunity (0. None, 1-3. Low, 3-5. Medium, 5-7. High, 8-10. Very High)')
 	st.markdown('**Economic Measures**: Governments support to debt relief and income support. (0. None, 1. Low, 2. Medium, 3. High)')
-	st.markdown('**Health Measures**: Governments action on contact tracing and testing policies (0. None, 1. Low, 2. Medium, 3. High)')
-	st.markdown('**Mobility Measures**: Governments restrictions in gatherings, lockdowns, restriction in internal movements, close public transport. (0. None, 1. Low, 2. Medium, 3. High)')
 	st.markdown('**Closure Measures**: Governments closure of workplaces, school and cancelation of public events. (0. None, 1. Low, 2. Medium, 3. High)')
 	st.markdown('**Holiday**: Number of holidays in the past week')
 	st.markdown('**Temperature**: Temperature in celsius')
@@ -28,18 +29,15 @@ if show_info:
 	st.markdown('**Urban Population**: Percentaje of the population that lives in urban areas')
 	st.markdown('**Vaccinations**: Number of vaccine doses administered per 100')
 	st.markdown('**Youth Unemployment**: Percentaje of youth unemployment')
-	st.markdown('**Infections**: An indicator of the level of virality in the current week')
-	st.markdown('**Accumulated cases**: Percentage of recovered individuals')
 
 
 st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
-    mobility_index = st.sidebar.slider('Bad Mobility', -100, 100, -20)
+    mobility_index = st.sidebar.slider('Mobility Index', -100, 100, -20)
     residential = st.sidebar.slider('Residential', -100, 100, 10)
-    awareness_measures = st.sidebar.slider('Awareness Measures', 0, 4, 1)
-    health_measures = st.sidebar.slider('Health Measures', 0, 3, 1)
-    mobility_measures = st.sidebar.slider('Mobility Measures',0, 3, 1)
+    infections_value = st.sidebar.slider('Infections', 0, 25, 4)
+    accumulated = st.sidebar.slider('Accumulated', 0, 10, 3)
     economic_measures = st.sidebar.slider('Economic Measures',0, 3, 1)
     closure_measures = st.sidebar.slider('Closure Measures',0, 3, 1)
     holiday = st.sidebar.slider('Holiday', 0, 7, 0)
@@ -49,15 +47,13 @@ def user_input_features():
     urban_population = st.sidebar.slider('Urban Population', 30, 100, 72)
     total_vaccinations_per_100 = st.sidebar.slider('Total Vaccinations per 100',0, 200, 0)
     youth_unemployment = st.sidebar.slider('Youth Unemployment',0, 35, 10)
-    infections_value = st.sidebar.slider('Infections', 0, 200, 20)
-    accumulated = st.sidebar.slider('Accumulated', 0, 100, 5)
     data = {'mobility_index': mobility_index,
             'residential': residential,
-            'awareness_measures': awareness_measures,
-            'health_measures': health_measures,
-            'mobility_measures': mobility_measures,
+            'awareness_measures': 1,
+            'health_measures': 1,
+            'mobility_measures': 1,
             'economic_measures': economic_measures,
-            'closure_measures': closure_measures,
+            'closure_measures': closure_measures+1,
             'holiday': holiday,
             'temp': temp,
             'prcp': prcp,
